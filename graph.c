cc3ccf623041003300f83ff6c80161e026f5b409
Jasmine Wang <jasminewang76@gmail.com>
2017-11-12 19:55:51 -0500

    finished bfs in C

--- graph.c
+++ graph.c
@@ -3,25 +3,61 @@
 
 void printVertices(int N, int G[][N]) {
     for (int i = 0; i < N; i++) {
-        printf("Vertex %d", i);
+        printf("Vertex %d\n", i);
     }
 }
 
 void printEdges(int N, int G[][N]) {
     //rows
-    printf("The graph has edges: ");
+    printf("The graph has edges:\n");
     for (int i = 0; i < N; i++) {
         //columns
         for (int j = 0; j < N; j++) {
             if (G[i][j] == 1) {
-                printf("[%d, %d]", i, j);
+                printf("[%d, %d]\n", i, j);
             }
         }
     }
 }
 
 void isThereAPath(int N, int G[][N], int v1, int v2) {
+    //visited array
+    int visited[N];
+    //queue implementation
+    int front = 0;
+    int rear = 0;
+    int q[N];
 
+    //make starting vertex as visited
+    visited[v1] = 1;
+    //enqueue starting vertex v
+    q[rear] = v1; //insert at rear
+    rear++; //increment rear
+
+    while (rear != front) // condition
+    {
+        //dequeue
+        int u = q[front];
+        front++;
+
+        //check
+        if (u == v2) {
+            printf("There is a path from Vertex %d to Vertex %d\n", v1, v2);
+            return;
+        }
+
+        int i = 0;
+        for (i = 0; i < N; i++) {
+            //if there is adjacent vertex enqueue it
+            if (!visited[i] && G[u][i]) {
+                q[rear] = i;
+                rear++;
+                visited[i] = 1;
+            }
+        }
+    }
+    //failure to find path
+    printf("There is no path from Vertex %d to Vertex %d\n", v1, v2);
 }
 
 #define N 5
